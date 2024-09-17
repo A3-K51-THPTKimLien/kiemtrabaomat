843cafc33f19cddf6bab9f579a6b2e7be9b2e748
A3-K51-THPTKimLien <tranletuananh918@gmail.com>
2024-09-17 07:07:59 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function checkPassword() {
+  var password = document.getElementById("password").value;
+  if (password === "a3mailatinhhoa") {
+    window.location.href = "https://a3-k51-thptkimlien.github.io/khochuaanh";
+  } else {
+    alert("Incorrect Password!");
+  }
+}
